16132b6c49f6f7bacb4227f2f28fba99a363b026
zhihang.wang <zhihang.wang@6c8d3a4c-d4d5-11dd-b6b4-918b84bbd919>
2009-05-17 16:24:56 +0000

    change the project structure

--- AVS_Transcoder_SDK/kernel/video/tavs/dec/inc/defines.h
+++ AVS_Transcoder_SDK/kernel/video/tavs/dec/inc/defines.h
@@ -1,37 +1,66 @@
+/*
+*****************************************************************************
+* COPYRIGHT AND WARRANTY INFORMATION
+*
+* Copyright 2003, Advanced Audio Video Coding Standard, Part II
+*
+* DISCLAIMER OF WARRANTY
+*
+* The contents of this file are subject to the Mozilla Public License
+* Version 1.1 (the "License"); you may not use this file except in
+* compliance with the License. You may obtain a copy of the License at
+* http://www.mozilla.org/MPL/
+*
+* Software distributed under the License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+* License for the specific language governing rights and limitations under
+* the License.
+*                     
+* THIS IS NOT A GRANT OF PATENT RIGHTS - SEE THE AVS PATENT POLICY.
+* The AVS Working Group doesn't represent or warrant that the programs
+* furnished here under are free of infringement of any third-party patents.
+* Commercial implementations of AVS, including shareware, may be
+* subject to royalty fees to patent holders. Information regarding
+* the AVS patent policy for standardization procedure is available at 
+* AVS Web site http://www.avs.org.cn. Patent Licensing is outside
+* of AVS Working Group.
+*
+* THIS IS NOT A GRANT OF PATENT RIGHTS - SEE THE AVS PATENT POLICY.
+************************************************************************
+*/
+
+/*
+*************************************************************************************
+* File name: 
+* Function: 
+*
+*************************************************************************************
+*/
+
 
 #ifndef _DEFINES_H_
 #define _DEFINES_H_
-//#define ROI_ENABLE
-#define AVS
-#define TRACE  0  /* !< 0:Trace off 1:Trace on */
+
 #define snprintf  _snprintf
-#define _THREE_STEP_MOTION_SEARCH_
-//#define _OUTPUT_DEC_IMG_
-//#define _ME_FOR_RATE_CONTROL_
 /*
  * define FastME ;
  * #define FIELDINTE
  */
+#define _THREE_STEP_MOTION_SEARCH_
 #define AVS_OUT_BUFFER_SIZE  (1024 * 1024 * 4)
 
 #define ET_SIZE        300  /* !< size of error text buffer */
-#define NUM_PIC_TYPE      5
+#define NUM_PIC_TYPE     5
 
 #define max(a, b)      (((a) > (b)) ? (a) : (b))
 #define min(a, b)      (((a) < (b)) ? (a) : (b))
 #define clamp(a, b, c)    ((a) < (b) ? (b) : ((a) > (c) ? (c) : (a)))  /* !< clamp a to the range of [b; c] */
 #define Clip3(min, max, val)  (((val) < (min)) ? (min) : (((val) > (max)) ? (max) : (val)))
 
-#define MAXMODE        13
-
-#define B8_SIZE 8
-#define MB_BLOCK_SIZE 16
-#define BLOCK_SIZE 4
-#define BLOCK_MULTIPLE (MB_BLOCK_SIZE / (2 * BLOCK_SIZE))
-
+#define MAXMODE            13
 #define INIT_FRAME_RATE    30
 #define EOSEOS      1    /* !< End Of Sequence */
 
 #define MAX_SYMBOLS_PER_MB  1200    /* !< Maximum number of different syntax elements for one MB */
-#define SVA_STREAM_BUF_SIZE 1024 
+#define STREAM_BUF_SIZE 1024 
 #endif
--- AVS_Transcoder_SDK/kernel/video/tavs/dec/inc/transcoder.h
+++ AVS_Transcoder_SDK/kernel/video/tavs/dec/inc/transcoder.h
@@ -6,25 +6,8 @@
 #include "transcoding_type.h"
 #include "global.h"
 
-//class c_mpeg2_dec
-//  {
-//  public:
-//    mpeg2_dec_create_t *p_mpeg2_dec_create;
-//    mpeg2_dec_frame_t  *p_mpeg2_dec_frame;
-//    mpeg2_dec_stats_t  *p_mpeg2_dec_stats;
-//    trans_frame_t      *p_trans_frame;    
-//  };
-
-//
-//DLL_EXPORT void transcoder_create  (c_avs_enc** p_avs_enc, c_mpeg2_dec* p_mpeg2_dec, int thread_num);
-//DLL_EXPORT void transcoder_decode  (c_mpeg2_dec* p_mpeg2_dec);
-//DLL_EXPORT void transcoder_encode  (c_avs_enc* p_avs_enc);
-//DLL_EXPORT void transcoder_destropy();
 void avs_encoder_create  (c_avs_enc* p_avs_enc);
 void avs_encoder_encode  (c_avs_enc* p_avs_enc);
 void avs_encoder_destroy(c_avs_enc* p_avs_enc);
-//int_32_t mpeg2_decoder_create( mpeg2_dec_create_t * create);
-//int_32_t mpeg2_decoder_destroy();
-//int_32_t mpeg2_decoder_decode( mpeg2_dec_frame_t * frame, mpeg2_dec_stats_t * stats);
 
 #endif
